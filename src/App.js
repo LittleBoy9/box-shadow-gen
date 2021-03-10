c073ed4ba44b51c418359fca142b4af7b2e33beb
LittleBoy <sounakdas9005@gmail.com>
2021-03-10 11:38:29 +0530

    box-shadow-gen

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/materialize-css/dist/css/materialize.min.css';
+import '../node_modules/materialize-css/dist/js/materialize.min.js';
+import './css/App.css';
+
+import Generator from './Generator';
+function App() {
+  return (
+    <>
+      <Generator />
+    </>
+  );
+}
+
+export default App;
